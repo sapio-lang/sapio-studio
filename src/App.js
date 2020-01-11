f04e0b6a2b14fe43c40b526760ab227c33347b8b
Jeremy Rubin <j@rubin.io>
2020-01-10 20:11:36 -0800

    Support more batchpay params

--- src/App.js
+++ src/App.js
@@ -166,25 +166,52 @@ class CreateBatchPayForm extends React.Component {
         if (radix === 0) {
             radix = amounts.size;
         }
-        let gas = this.form.gas.valueAsNumber || 0;
-        this.props.vaultman.create_batchpay({amounts:Object.fromEntries(amounts.entries()), radix, gas});
+        const gas = this.form.gas.valueAsNumber || 0;
+        const pairing_mode = this.form.pairing_mode.value;
+        this.props.vaultman.create_batchpay({amounts:Object.fromEntries(amounts.entries()), radix, gas, pairing_mode});
         this.props.hide();
 
     };
     render() {
         return(
-                <Form  onSubmit={this.handleSubmit.bind(this)}>
+            <Form  onSubmit={this.handleSubmit.bind(this)}>
+                <Form.Group>
+                    <Form.Label> Payments to Make </Form.Label>
                     <Form.Control as="textarea" placeholder="bcrt1q0548jkmkzksch8hc367jm77up40yydqh87e3qa 1.4"
                         ref={(amt) => this.form.amount = amt} rows="6"/>
-                    <FormControl type="number" placeholder="Radix -- n for n, 1 for linear, 0 for packed, -n for at most n descendants" className=" mr-sm-1"
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Radix </Form.Label>
+                    <Form.Text className="text-muted">
+                        Radix -- n for n, 1 for linear, 0 for packed, -n for at most n descendants.
+                    </Form.Text>
+                    <FormControl type="number" placeholder="4" className=" mr-sm-1"
                         ref={r => this.form.radix = r}
-                        min="-1" max="21000000" step="1"/>
-
-                    <FormControl type="number" placeholder="gas ports" className=" mr-sm-1"
+                        max="21000000" step="1"/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Gas Ports to Include </Form.Label>
+                    <Form.Text className="text-muted">
+                        value must be unspecified or above or equal to 472.
+                    </Form.Text>
+                    <FormControl type="number" placeholder="None" className=" mr-sm-1"
                         ref={r => this.form.gas = r}
                         min="472" max="14610" step="1"/>
-                    <Button  type="submit" >Submit</Button>
-                </Form>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label> Output Sorting Method </Form.Label>
+                    <FormControl as="select" className=" mr-sm-2"
+                        ref={r => this.form.pairing_mode = r} >
+                        <option value="AS_IS">As Is</option>
+                        <option value="VALUE">Group By Value</option>
+                        <option value="BALANCE_VALUE">Balance By Value</option>
+                        <option value="PROBABILITY">Group By Probability</option>
+                        <option value="BALANCE_PROBABILITY">Balance By Probability</option>
+                        <option value="LEXICOGRAPHICAL">Lexicographical</option>
+                    </FormControl>
+                </Form.Group>
+                <Button  type="submit" >Submit</Button>
+            </Form>
 
         );
     }
@@ -199,19 +226,25 @@ class CreateVaultModal extends React.Component {
 
     render() {
         return(
-            <Modal show={this.props.show} onHide={this.props.hide}>
+            <Modal show={this.props.show} onHide={this.props.hide} size="lg">
                 <Modal.Header closeButton>
                     <Modal.Title> Create a New Vault </Modal.Title>
                 </Modal.Header>
-                <Tabs defaultActiveKey="vault" variant="pills">
-                    <Tab eventKey="vault" title="Vault">
-                        <CreateVaultForm hide={this.props.hide} vaultman={this.props.vaultman}/>
-                    </Tab>
-                    <Tab eventKey="batchpay" title="BatchPay">
 
-                        <CreateBatchPayForm hide={this.props.hide} vaultman={this.props.vaultman}/>
-                    </Tab>
-                </Tabs>
+                <Tab.Container defaultActiveKey="vault">
+                        <Nav variant="tabs" justify className="navbar">
+                            <Nav.Item> <Nav.Link eventKey="vault"> Vault </Nav.Link> </Nav.Item>
+                            <Nav.Item> <Nav.Link eventKey="batchpay"> BatchPay </Nav.Link> </Nav.Item>
+                        </Nav>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="vault" title="Vault">
+                            <CreateVaultForm hide={this.props.hide} vaultman={this.props.vaultman}/>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="batchpay" title="BatchPay">
+                            <CreateBatchPayForm hide={this.props.hide} vaultman={this.props.vaultman}/>
+                        </Tab.Pane>
+                    </Tab.Content>
+                </Tab.Container>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={this.props.hide}> Close </Button>
                 </Modal.Footer>
