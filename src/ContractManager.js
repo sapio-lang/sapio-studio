f36c1e6af168bf380d25507ee870c8bb5d7380fb
Jeremy Rubin <j@rubin.io>
2020-04-15 13:50:53 -0700

    clean up contract manager

--- src/ContractManager.js
+++ src/ContractManager.js
@@ -24,48 +24,58 @@ function preprocess_data(data) {
 
     return {txns: txns,  txn_colors: txn_colors, txn_labels:txn_labels, utxo_labels};
 }
+
+function process_inputs_map(txns, inputs_map) {
+    for (let x = 0; x < txns.length; ++x) {
+        const txn = txns[x];
+        for (let y = 0; y < txn.ins.length; ++y) {
+            const key = keyFn(txn.ins[y]);
+            let arr = inputs_map.get(key) || [];
+            arr.push(x);
+            inputs_map.set(key, arr);
+        }
+    }
+}
+
+function process_txn_models(txns, txid_map, update, txn_labels, txn_colors, utxo_labels, txn_models) {
+    for (let x = 0; x < txns.length; ++x) {
+        const txn = txns[x];
+        txid_map.set(txn.getTXID(), x);
+        const txn_model = new TransactionModel(txn, update, txn_labels[x], txn_colors[x], utxo_labels[x]);
+        txn_models.push(txn_model);
+    }
+}
+function process_utxo_models(txns, txn_models, inputs_map, to_add) {
+    for (let x = 0; x < txns.length; ++x) {
+        const txn = txns[x];
+        const len = txn.outs.length;
+        for (let y = 0; y < len; ++y) {
+            const utxo_model = txn_models[x].utxo_models[y];
+            let key = keyFn({ hash: txns[x].getHash(), index: y });
+            let idxs = inputs_map.get(key) || [];
+            for (let z = 0; z < idxs.length; ++z) {
+                const spender = txn_models[idxs[z]];
+                const idx = spender.tx.ins.findIndex(elt => elt.index === y && elt.hash.toString('hex') === txn.getHash().toString('hex'));
+                const link = utxo_model.addOutPort('spend ' + z).link(spender.addInPort('input' + idx));
+                spender.input_links.push(link);
+                utxo_model.utxo.spends.push(spender);
+                to_add.push(link);
+            }
+        }
+        to_add.push(...txn_models[x].utxo_models);
+        to_add.push(...txn_models[x].utxo_links);
+    }
+}
 function process_data(update, obj) {
     let {txns, txn_colors, txn_labels, utxo_labels} =obj;
 	let inputs_map = new Map();
-	for (let x = 0; x < txns.length; ++x) {
-		const txn = txns[x];
-		for (let y = 0; y < txn.ins.length; ++y) {
-			const key = keyFn(txn.ins[y]);
-			let arr = inputs_map.get(key) || [];
-			arr.push(x);
-			inputs_map.set(key, arr);
-		}
-	}
+	process_inputs_map(txns, inputs_map);
 
 	let txid_map = new Map();
 	let txn_models = [];
-	for (let x = 0; x < txns.length; ++x) {
-		const txn = txns[x];
-        txid_map.set(txn.getTXID(), x);
-        const txn_model = new TransactionModel(txn, update, txn_labels[x], txn_colors[x], utxo_labels[x]);
-		txn_models.push(txn_model);
-    }
+	process_txn_models(txns, txid_map, update, txn_labels, txn_colors, utxo_labels, txn_models);
 	const to_add = [];
-	for (let x = 0; x < txns.length; ++x) {
-        const txn = txns[x];
-        const len = txn.outs.length;
-		for (let y = 0; y < len; ++y) {
-            const utxo_model = txn_models[x].utxo_models[y];
-			let key = keyFn({hash: txns[x].getHash(), index: y});
-			let idxs = inputs_map.get(key) || [];
-
-			for (let z = 0; z < idxs.length; ++z) {
-				const spender = txn_models[idxs[z]];
-                const idx = spender.tx.ins.findIndex(elt => elt.index === y && elt.hash.toString('hex') === txn.getHash().toString('hex'));
-                const link = utxo_model.addOutPort('spend '+z).link(spender.addInPort( 'input' + idx));
-                spender.input_links.push(link);
-                utxo_model.utxo.spends.push(spender);
-				to_add.push(link);
-			}
-		}
-        to_add.push(...txn_models[x].utxo_models);
-        to_add.push(...txn_models[x].utxo_links);
-	}
+	process_utxo_models(txns, txn_models, inputs_map, to_add);
     return {inputs_map:inputs_map, utxo_models:to_add, txn_models:txn_models, txid_map:txid_map};
 }
 
@@ -112,3 +122,6 @@ export class ContractModel extends ContractBase {
         });
     }
 }
+
+
+
