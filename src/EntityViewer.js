06c82724bd57efc2326b535b960bf5ecc1c02ccb
Jeremy Rubin <j@rubin.io>
2020-04-16 12:18:39 -0700

    Clean up EntityViewer with switch statement

--- src/EntityViewer.js
+++ src/EntityViewer.js
@@ -8,20 +8,27 @@ export class EntityViewer extends React.Component {
         this.state = {};
     }
     render() {
-        const transaction_component = this.props.entity.type === "txn" ?
-            <TransactionComponent entity={this.props.entity} hide_details={this.props.hide_details} update={this.props.update_viewer} find_tx_model={(txid, n) => {
-                const idx = this.props.current_contract.txid_map.get(hash_to_hex(txid));
-                if (idx === undefined)
-                    return null;
-                return this.props.current_contract.txn_models[idx].utxo_models[n];
-            }} />
-            : null;
-        const utxo_component = this.props.entity.type === "utxo" ?
-            <UTXOComponent entity={this.props.entity} hide_details={this.props.hide_details} update={this.props.update_viewer} />
-            : null;
-        return (<>
-            {transaction_component}
-            {utxo_component}
-        </>);
+        switch (this.props.entity.type) {
+            case "txn":
+                return (<TransactionComponent
+                    entity={this.props.entity}
+                    hide_details={this.props.hide_details}
+                    update={this.props.update_viewer}
+                    find_tx_model={(txid, n) => {
+                        const idx = this.props.current_contract.txid_map.get(hash_to_hex(txid));
+                        if (idx === undefined)
+                            return null;
+                        return this.props.current_contract.txn_models[idx].utxo_models[n];
+                    }} />
+
+                );
+            case "utxo":
+                return (<UTXOComponent
+                    entity={this.props.entity}
+                    hide_details={this.props.hide_details}
+                    update={this.props.update_viewer} />)
+            default:
+                return null;
+        }
     }
 }
