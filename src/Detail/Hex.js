6cd3595cc2e8ce1c25fafbc06d0f015e6046c5a5
Jeremy Rubin <j@rubin.io>
2020-08-20 12:37:08 -0700

    copy hex fields on doubleclick and show tooltip with copy notice

--- src/Detail/Hex.js
+++ src/Detail/Hex.js
@@ -1,18 +1,54 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-export default class Hex extends React.Component {
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+class BaseHex extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {};
     }
     copy() {
-
-        this.code.select();
-
+        const select = window.getSelection();
+        select.removeAllRanges();
+        const range=document.createRange();
+        range.selectNodeContents(this.code);
+        select.addRange(range);
+        const txt = select.toString().trim();
+        navigator.clipboard.writeText(txt);
+        this.setState({tip: "copied!"});
+        setTimeout(()=>{
+            this.setState({ tip: undefined });
+        }, 1000);
     }
     render() {
-        return (<>
-            <code className="truncate" ref={(r) => this.code =r}>{this.props.value} </code>
-        </>)
+        return (
+
+            <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>{this.state.tip || "double click to copy."}</Tooltip>}
+            >
+                    <code className={this.styling} ref={(r) => this.code = r}>{this.props.value} </code>
+            </OverlayTrigger>
+
+        )
+    }
+    componentDidMount() {
+        this.code.addEventListener('dblclick',
+            this.copy.bind(this)
+        )
+    }
+}
+
+export default class Hex extends BaseHex{
+    constructor(props) {
+        super(props);
+        this.styling = "truncate";
+    }
+}
+
+export class ASM extends BaseHex{
+    constructor(props) {
+        super(props);
+        this.styling = "ASM";
     }
 }
 export function hash_to_hex(h) {
@@ -21,18 +57,3 @@ export function hash_to_hex(h) {
     b.reverse();
     return b.toString('hex');
 };
-export class ASM extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    copy() {
-
-        this.code.select();
-
-    }
-    render() {
-        return (<>
-            <code className="ASM" ref={(r) => this.code =r}>{this.props.value} </code>
-        </>)
-    }
-}
