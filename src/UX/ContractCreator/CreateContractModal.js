e743aab6c6eb165161d5135bbad8c5fd3ee636c8
Jeremy Rubin <j@rubin.io>
2021-08-11 21:55:05 -0700

    Move AppSelectors

--- src/UX/ContractCreator/CreateContractModal.js
+++ src/UX/ContractCreator/CreateContractModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import { Menu } from '../Compiler/Menu';
+import { Menu } from './Menu';
 
 export class CreateContractModal extends React.Component {
     constructor(props) {
