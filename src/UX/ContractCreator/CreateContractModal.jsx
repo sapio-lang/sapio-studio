d12f5174a55998bc5ae4b1d5e37cf1af45e8e5fd
Jeremy Rubin <j@rubin.io>
2021-08-25 13:40:19 -0700

    Simplify create contract calls

--- src/UX/ContractCreator/CreateContractModal.jsx
+++ src/UX/ContractCreator/CreateContractModal.jsx
@@ -1,29 +1,20 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { PluginSelector } from './SapioPluginPicker/PluginSelector';
 
-export class CreateContractModal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.form = {};
-    }
-    render() {
-        if (!this.props.dynamic_forms) return null;
-        return (
-            <Modal show={this.props.show} onHide={this.props.hide} size="lg">
-                <Modal.Header closeButton>
-                    <Modal.Title> Applications </Modal.Title>
-                </Modal.Header>
+export function CreateContractModal(props) {
+    if (!props.dynamic_forms) return null;
+    return (
+        <Modal show={props.show} onHide={props.hide} size="lg">
+            <Modal.Header closeButton>
+                <Modal.Title> Applications </Modal.Title>
+            </Modal.Header>
 
-                <PluginSelector
-                    hide={this.props.hide}
-                    load_new_model={this.props.load_new_model}
-                    applications={this.props.dynamic_forms}
-                    export
-                    compiler={this.props.compiler}
-                />
-            </Modal>
-        );
-    }
+            <PluginSelector
+                hide={props.hide}
+                applications={props.dynamic_forms}
+                export
+                compiler={props.compiler}
+            />
+        </Modal>
+    );
 }
